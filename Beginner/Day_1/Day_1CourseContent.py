a861e79f30836a7f3b3d4831a5ce0fb04ccc7372
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-03-30 10:46:08 +0000

    Create Day_1CourseContent.py

---
+++ Beginner/Day_1/Day_1CourseContent.py
@@ -0,0 +1,59 @@
+##### Printing #####
+
+# print("A 'single quote' inside a double quote")
+# print('A "double quote" inside a single quote')
+# print("Alternatively you can juste \"escape\" the quote")
+
+##### String Manipulation #####
+
+# print("Hello World!\nHello World!\nHello world!")
+
+# name = "AFCM"
+
+# print("Hello" + " " + name)
+
+##### The Python Input Function #####
+
+# print("Hello " + input("What is your name? "))
+
+# name = input("What's your name?: ")
+# len(name)
+# print(len(name))
+
+# name = input(())
+# len(name)
+# print(len(name))
+
+# print(len(input()))
+
+##### Python Variables #####
+
+# name = input("What is your name?")
+# print(name)
+
+# name = "Jack"
+# print(name)
+
+# print(len(input("What is your name?"))) #long line of code
+#use of variables
+# name = input('What\'s your name?')
+# print(len(name))
+
+# name = input("What\'s your name?")
+# length = len(name)
+# print(length)
+
+
+# There are two variables, a and b from input
+# a = input()
+# b = input()
+# # 🚨 Don't change the code above ☝️
+# ####################################
+# # Write your code below this line 👇
+# c = a # c = Hello
+# a = b # a = World
+# b = c # b = Hello
+
+# 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
