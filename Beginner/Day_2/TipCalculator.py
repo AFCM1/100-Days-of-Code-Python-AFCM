7984d913e838a11c98116902e65c8ddbd2b8dc9d
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-04-03 11:13:18 +0000

    Update TipCalculator.py

--- Beginner/Day_2/TipCalculator.py
+++ Beginner/Day_2/TipCalculator.py
@@ -9,8 +9,7 @@
 
 #My version
 print("Welcome to the tip calculator!")
-total = float(input("What was the total bill? "))
-print(f"${total}")
+total = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12 or 15? "))
 split = int(input("How many people to split the bill? "))
 tip2 = float(tip / 100)
@@ -18,3 +17,15 @@ total_tip = total * tip2
 amount = (total + total_tip) / split
 amount = round(amount, 2)
 print(f"Each person should pay: ${amount}")
+
+#Angela version
+# print("Welcome to the tip calculator!")
+# bill = float(input("What was the total bill? $"))
+# tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+# people = int(input("How many people to split the bill?"))
+
+# tip_as_percent = tip / 100
+# total_tip_amount = bill * tip_as_percent
+# total_bill = bill + total_tip_amount
+# bill_per_person = total_bill / people
+# final_amount = round(bill_per_person, 2)
