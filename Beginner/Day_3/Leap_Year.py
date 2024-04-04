93e5934540772f9cae2672ae6080ce5e9318d466
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-04-04 16:33:24 +0000

    Create Leap_Year.py

---
+++ Beginner/Day_3/Leap_Year.py
@@ -0,0 +1,15 @@
+# Which year do you want to check?
+year = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+Divisible by 4 ? 
+No = Not leap
+Yes : 
+  Divisible by 100 ?
+    No = Leap
+    Yes :
+      Divisible by 400 ?
+        No = No Leap
+        Yes = Leap
