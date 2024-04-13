fc1107cfac6a9c8a994180f63c822870bfd68785
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-04-13 19:12:40 +0000

    Create Treasure_Map.py

---
+++ Beginner/Day_4/Treasure_Map.py
@@ -0,0 +1,25 @@
+#NOT SUCCEEDED
+
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input() # Where do you want to put the treasure?
+# 🚨 Don't change the code above 👆
+# Write your code below this row 👇
+
+letter = position[0].lower()
+print(letter)
+
+abc = ["a", "b", "c"]
+#to do the comparison
+letter_index = abc.index(letter)
+#print(letter_index)
+number_index = int(position[1])-1
+#print(f"{number_index}")
+map[number_index][letter_index] = "X"
+
+# Write your code above this row 👆
+# 🚨 Don't change the code below 👇
+print(f"{line1}\n{line2}\n{line3}")
