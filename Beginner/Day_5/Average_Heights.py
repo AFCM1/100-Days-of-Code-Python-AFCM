6ee19d37b65632c1f4dcb0926cfca143fe56034e
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-04-14 13:13:00 +0000

    Update Average_Heights.py

--- Beginner/Day_5/Average_Heights.py
+++ Beginner/Day_5/Average_Heights.py
@@ -1,5 +1,3 @@
-#My version
-
 # Input a Python list of student heights
 student_heights = input().split()
 for n in range(0, len(student_heights)):
@@ -7,4 +5,41 @@ for n in range(0, len(student_heights)):
 # 🚨 Don't change the code above 👆
   
 # Write your code below this row 👇
+#print(student_heights[n]) #affiche le dernier item de la liste
 
+total_height = 0
+num_students = 0
+average_height = 0
+
+for height in student_heights:
+  total_height += height
+
+for student in student_heights:
+  num_students += 1
+
+average_height = total_height / num_students
+average_height = round(average_height)
+
+print(f"total height = {total_height}")
+print(f"number of students = {num_students}")
+print(f"average height = {average_height}")
+
+#Angela version
+
+# Input a Python list of student heights
+student_heights = input().split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# Your code below this row 👇
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(f"total height = {total_height}")
+
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+print(f"number of students = {number_of_students}")
+
+average_height = round(total_height / number_of_students)
+print(f"average height = {average_height}")
