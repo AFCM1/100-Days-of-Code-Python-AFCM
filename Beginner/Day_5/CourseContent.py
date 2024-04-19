837b55f1df903c9112cc0bff6675941e9230fbba
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-04-19 12:35:47 +0000

    Create CourseContent.py

---
+++ Beginner/Day_5/CourseContent.py
@@ -0,0 +1,31 @@
+#for item in list_of_items:
+  #Do something to each item
+# nb = 0
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+  #print(fruit)
+#   print(fruit + " Pie")
+
+# for fruit in fruits:
+#   nb += 1
+
+# print(f"total fruits in list is {nb}")
+#print(fruits)
+
+### for loops and the range() function
+
+# for number in range (1, 11, 3):
+#   print(number)
+
+
+# total = 0 
+# for number in range(1, 101):
+#   total += number
+# print(total)
+
+
+
+
+    
+
+
