e99bb6db8809699ccbcb21960f4fa43fe55de315
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-04-20 08:28:45 +0000

    Update The_hurdles_loop_challenge.py

--- Beginner/Day_6/The_hurdles_loop_challenge.py
+++ Beginner/Day_6/The_hurdles_loop_challenge.py
@@ -35,3 +35,35 @@ def turn_right_move():
     move()  
 
 jump()
+
+
+#Shorter version
+
+def jump():
+    move()
+    jump_func()
+    jump_func()
+    jump_func()
+    jump_func()
+    jump_func()
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    
+def turn_left_move():
+    turn_left()
+    move()
+
+def turn_right_move():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()  
+    
+def jump_func():
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    turn_left_move()
+
+jump()
