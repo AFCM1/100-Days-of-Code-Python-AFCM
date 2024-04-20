92a483775a3b60cf22ee089b498f0ff99bac9076
AFCM <32823650+AFCM1@users.noreply.github.com>
2024-04-20 08:21:13 +0000

    Create The_hurdles_loop_challenge.py

---
+++ Beginner/Day_6/The_hurdles_loop_challenge.py
@@ -0,0 +1,37 @@
+def jump():
+    move()
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    turn_left_move()
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    turn_left_move()
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    turn_left_move()
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    turn_left_move()
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    turn_left_move()
+    turn_left_move()
+    turn_right_move()
+    turn_right_move()
+    
+def turn_left_move():
+    turn_left()
+    move()
+
+def turn_right_move():
+    turn_left()
+    turn_left()
+    turn_left()
+    move()  
+
+jump()
